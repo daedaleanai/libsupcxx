5bde9d54a0583454a29b1aafb44e418d0b6cb7d7
Lukasz Janyst <lj@ddln.ai>
2018-12-25 20:29:10 +0100

    tests: Add a simple test for virtuals

---
+++ tests/test-02-virtuals.cc
@@ -0,0 +1,35 @@
+
+#include <io/printf.hh>
+
+class IInterface {
+public:
+  virtual void doStuff() = 0;
+};
+
+class Impl1: public IInterface {
+public:
+  virtual void doStuff() override {
+    io::printf("Do stuff #1\n");
+  }
+};
+
+class Impl2: public IInterface {
+public:
+  virtual void doStuff() override {
+    io::printf("Do stuff #2\n");
+  }
+};
+
+void doStuff(IInterface *a) {
+  a->doStuff();
+}
+
+void main(const char *cmdline) {
+  Impl1 a;
+  Impl2 b;
+
+  io::printf("Call doStuff(&a)\n");
+  doStuff(&a);
+  io::printf("Call doStuff(&b)\n");
+  doStuff(&b);
+}
