f3b1b81573f344fedcfa239f53a32d5019166b48
Lukasz Janyst <lj@ddln.ai>
2019-01-03 18:16:13 +0100

    tests: Add a simple numeric_limits test

---
+++ tests/test-09-numeric-limits.cc
@@ -0,0 +1,11 @@
+
+#include <io/printf.hh>
+
+#include <limits>
+#include <cstdint>
+
+void main(const char *cmdline) {
+  io::printf("Hello numeric_limits\n");
+  io::printf("std::numeric_limits<std::uint64_t>::max(): %llu\n",
+             std::numeric_limits<std::uint64_t>::max());
+}
