214f17a9f63c8962e14fb0613b884fc8c10c2d6f
Lukasz Janyst <lj@ddln.ai>
2019-01-04 13:29:20 +0100

    tests: Add digits test to numeric-limits

--- tests/test-09-numeric-limits.cc
+++ tests/test-09-numeric-limits.cc
@@ -8,4 +8,7 @@ void main(const char *cmdline) {
   io::printf("Hello numeric_limits\n");
   io::printf("std::numeric_limits<std::uint64_t>::max(): %llu\n",
              std::numeric_limits<std::uint64_t>::max());
+  io::printf("std::numeric_limits<std::uint64_t>::digits: %d\n",
+             std::numeric_limits<std::uint64_t>::digits);
+
 }
