a9ab658cc3a6ef373c25d399238522b4928efa6d
Lukasz Janyst <lj@ddln.ai>
2019-01-04 13:38:11 +0100

    tests: Add a simple type_traits test

---
+++ tests/test-10-type-traits.cc
@@ -0,0 +1,16 @@
+
+#include <io/printf.hh>
+
+#include <type_traits>
+#include <cstdint>
+
+struct Test {
+  std::uint16_t test = 42;
+};
+
+void main(const char *cmdline) {
+  io::printf("Is int tivially assignable to const Test: %d!\n",
+             std::is_trivially_assignable<const Test, int>::value);
+  io::printf("Is const Test& trivially assignable to Test&: %d!\n",
+             std::is_trivially_assignable<Test&, const Test&>::value);
+}
